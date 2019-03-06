d9236e181adeacf02ed5077bf779efd51a3c6d3b
AlfieriChou <alfierichou@gmail.com>
2019-03-06 12:27:28 +0800

    release v1.0.2

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # grpc-exception
+
 gRPC exception in node.js
+
+## Features
+
+- Some Exceptions are already implemented:
+
+  - Ok
+  - Cancelled
+  - Unknow
+  - InvalidArgument
+  - DeadlineExceeded
+  - NotFound
+  - AlreadyExists
+  - PermissionDenied
+  - ResourceExhausted
+  - FailedPrecondition
+  - Aborted
+  - OutOfRange
+  - Unimplemented
+  - Internal
+  - Unavailable
+  - DataLoss
+  - Unauthenticated
--- src/index.ts
+++ src/index.ts
@@ -42,3 +42,84 @@ export class Unknown extends Exception {
     super(STATUS, message)
   }
 }
+
+export class FailedPrecondition extends Exception {
+  constructor(
+    STATUS = status.FAILED_PRECONDITION,
+    message = 'failed precondition'
+  ) {
+    super(STATUS, message)
+  }
+}
+
+export class Internal extends Exception {
+  constructor(STATUS = status.INTERNAL, message = 'Internal') {
+    super(STATUS, message)
+  }
+}
+
+export class Ok extends Exception {
+  constructor(STATUS = status.OK, message = 'ok') {
+    super(STATUS, message)
+  }
+}
+
+export class OutOfRange extends Exception {
+  constructor(STATUS = status.OUT_OF_RANGE, message = 'out of range') {
+    super(STATUS, message)
+  }
+}
+
+export class ResourceExhausted extends Exception {
+  constructor(
+    STATUS = status.RESOURCE_EXHAUSTED,
+    message = 'resource exhausted'
+  ) {
+    super(STATUS, message)
+  }
+}
+
+export class AlreadyExists extends Exception {
+  constructor(STATUS = status.ALREADY_EXISTS, message = 'already exists') {
+    super(STATUS, message)
+  }
+}
+
+export class Cancelled extends Exception {
+  constructor(STATUS = status.CANCELLED, message = 'Cancelled') {
+    super(STATUS, message)
+  }
+}
+
+export class Aborted extends Exception {
+  constructor(STATUS = status.ABORTED, message = 'aborted') {
+    super(STATUS, message)
+  }
+}
+
+export class DataLoss extends Exception {
+  constructor(STATUS = status.DATA_LOSS, message = 'data loss') {
+    super(STATUS, message)
+  }
+}
+
+export class DeadlineExceeded extends Exception {
+  constructor(
+    STATUS = status.DEADLINE_EXCEEDED,
+    message = 'deadline exceeded'
+  ) {
+    super(STATUS, message)
+  }
+}
+
+export class Unavailable extends Exception {
+  constructor(STATUS = status.UNAVAILABLE, message = 'unavailable') {
+    super(STATUS, message)
+  }
+}
+
+export class Unimplemented extends Exception {
+  constructor(STATUS = status.UNIMPLEMENTED, message = 'unimplemented') {
+    super(STATUS, message)
+  }
+}
